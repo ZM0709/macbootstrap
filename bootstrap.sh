c35e1b8003db7dcdf589079b596c37c49ff70f07
zhanggm79 <zhanggm79@users.noreply.github.com>
2021-01-26 16:46:14 +0800

    。

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,17 @@
 #!bin/bash
-[ -z "${BRANCH}" ] && export BRANCH="master"
+# [ -z "${BRANCH}" ] && export BRANCH="master"
 
-if [[ -e ~/.macbootstrap ]]; then
-  rm -rf ~/.macbootstrap
-fi
+# if [[ -e ~/.macbootstrap ]]; then
+#   rm -rf ~/.macbootstrap
+# fi
 
-if [[ ! -e /usr/local/bin/brew ]]; then
-  /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
-else
-    echo "You have installed brew"
-fi
+# if [[ ! -e /usr/local/bin/brew ]]; then
+#   /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+# else
+#     echo "You have installed brew"
+# fi
 
-brew install git
+# brew install git
 
 git clone --depth=1 -b ${BRANCH} https://github.com/zhanggm79/macbootstrap.git ~/.macbootstrap
 cd ~/.macbootstrap
